4e6e687c88158689b00210cbc226a65f56e7ee81
Fabi <fabiane1974@hotmail.com>
2026-09-06 13:45:58 -0300

    commit inicial

---
+++ src/main/java/com/fabianeSilva/cadastro_cursos/CadastroCursosApplication.java
@@ -0,0 +1,13 @@
+package com.fabianeSilva.cadastro_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabianeSilva/cadastro_cursos/CadastroCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabianeSilva.cadastro_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
